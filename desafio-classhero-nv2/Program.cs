455dee6ddb4d117c158330d31e18dab466cd10e4
habner-gabryel <habner.gabryel.cc@gmail.com>
2025-08-28 22:15:24 -0300

    Resposta do desafio 2

--- desafio-classhero-nv2/Program.cs
+++ desafio-classhero-nv2/Program.cs
@@ -1,10 +1,14 @@
 ﻿class Program {
     static void Main()
     {
-        double a = 0.1 + 0.2;
-        double b = 0.3;
+        double notaProva = 8.5;
+        int quantAlunos = 40;
+        string nomeCurso = "Ciência da Computação";
+        char statusPagamento = 'P';
 
-        Console.WriteLine(a == b);
-        Console.WriteLine(a + " vs " + b);
+        Console.WriteLine("Nota da Prova: " + notaProva);
+        Console.WriteLine("Quantidade de Alunos: " + quantAlunos);
+        Console.WriteLine("Nome do Curso: " + nomeCurso);
+        Console.WriteLine("Status de Pagamento: " + statusPagamento);
     }
 }
