28a5747ce9864259fc30f70431a166ff36d15b24
habner-gabryel <habner.gabryel.cc@gmail.com>
2025-08-28 22:03:22 -0300

    Criando projeto para o desafio da missão 02.

---
+++ desafio-classhero-nv2/Program.cs
@@ -0,0 +1,7 @@
+﻿class Program {
+    static void Main()
+    {
+        double resultado = 0.1 + 0.2 + 0.3;
+        Console.WriteLine("Resultado da soma 0.1 + 0.2 + 0.3 = " + resultado);
+    }
+}
