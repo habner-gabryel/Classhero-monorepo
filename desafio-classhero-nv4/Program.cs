4f44ebbd98ae3dc131f92b0eebbecad47fe50c5b
habner-gabryel <habner.gabryel.cc@gmail.com>
2025-09-08 10:38:26 -0300

    Adicionando desafio do nível 4

---
+++ desafio-classhero-nv4/Program.cs
@@ -0,0 +1,27 @@
+﻿class Program
+{
+    static void Main()
+    {
+        double media = 0D;
+
+        for (int i = 1; i <= 4; i++)
+        {
+            Console.WriteLine("Informe a " + i + "ª nota: ");
+            double nota = double.Parse(Console.ReadLine());
+
+            media += nota;
+        }
+
+        media /= 4;
+
+        if (media >= 6)
+        {
+            Console.WriteLine("Aluno aprovado!");
+        }
+        else
+        {
+            Console.WriteLine("Aluno Reprovado.");
+        }
+        Console.WriteLine("Média: " + media);
+    }
+}
